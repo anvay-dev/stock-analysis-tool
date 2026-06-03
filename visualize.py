1e307035e6a72e097d45a3becef5a4e9b945fadd
Anvay Wadhwa <aannvvaayy@gmail.com>
2026-06-03 19:24:30 +0700

    add daily return, volatility, and RSI features to fetch_data.py

--- visualize.py
+++ visualize.py
@@ -4,7 +4,6 @@ from fetch_data import aapl_data
 plt.plot(aapl_data["Close"], label="Close")
 plt.plot(aapl_data["MA_10"], label="MA_10")
 plt.plot(aapl_data["MA_30"], label="MA_30")
-plt.plot(aapl_data["Daily Return"], label = "Daily Return")
 
 plt.title("AAPL Price + Moving Averages")
 plt.xlabel("Date")
