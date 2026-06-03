8921a622b636ed81f8b515447c5d3ba87c1ef0c4
Anvay Wadhwa <aannvvaayy@gmail.com>
2026-06-03 16:11:11 +0700

    add visualize.py with price and moving average chart

--- visualize.py
+++ visualize.py
@@ -1,1 +1,12 @@
+import matplotlib.pyplot as plt
+from fetch_data import aapl_data
 
+plt.plot(aapl_data["Close"], label="Close")
+plt.plot(aapl_data["MA_10"], label="MA_10")
+plt.plot(aapl_data["MA_30"], label="MA_30")
+
+plt.title("AAPL Price + Moving Averages")
+plt.xlabel("Date")
+plt.ylabel("Price (USD)")
+plt.legend()
+plt.show()
